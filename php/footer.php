7b1d54ef25eaf614aeeb056d7a5759c739aa93b2
Rinsevych <urarin20@gmail.com>
2018-03-12 07:25:18 +0200

    +
    +

---
+++ php/footer.php
@@ -0,0 +1,71 @@
+<section class="cid-qA5ZMwCHhs" id="footer1-g" data-rv-view="56">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row content text-white">
+            <div class="col-12 col-md-3">
+                <div class="media-wrap">
+                    <a href="https://zavadivkaschool.github.io">
+                        <img src="assets/images/log.png" alt="Завадівський НВК" style="width: 58%;" media-simple="true">
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Адреса
+                </h5>
+                <p class="mbr-text">
+                    Київська область, Володарський район, 
+                    <br>Село Завадівка, вул. Шкільна 35,
+                    індекс 09350
+                </p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Контакти
+                </h5>
+                <p class="mbr-text">
+                    Email: osvita_zav@ukr.net
+                    <br>Телефон: (045)-69-5-78-41
+                </p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                  Посилання
+                </h5>
+                <p class="mbr-text">
+                    <a class="text-primary" href="https://zavadivkaschool.github.io.">Завадівська школа</a>
+               
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-sm-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row mbr-white">
+                <div class="col-sm-6 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        © Copyright 2017 YR - All Rights Reserved
+                    </p>
+                </div>
+                <div class="col-md-1">
+                    
+                        <div class="soc-item">
+                            <a href="https://plus.google.com/u/2/104255587530578985617" target="_blank">
+                                <span class="socicon-googleplus socicon mbr-iconfont mbr-iconfont-social" media-simple="true"></span>
+                            </a>
+                        </div>
+                        
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> 
---
+++ php/menu.php
@@ -0,0 +1,85 @@
+<section class="menu cid-qA5ZMu2E14" once="menu" id="menu1-e" data-rv-view="51">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="https://zavadivkaschool.github.io">
+                         <img src="assets/images/log.png" alt="zavadivkaschool" media-simple="true" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap">
+                    <a class="navbar-caption text-white display-4" href="https://zavadivkaschool.github.io">
+                         КЗ «ЗАВАДІВСЬКИЙ НВК «ЗОШ І-ІІІ СТ.–Д/С»
+                    </a>
+                </span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="#features17-n">
+                        <span class="mbri-calendar mbr-iconfont mbr-iconfont-btn"></span>
+                        Новини
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="history.html">
+                        <span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span>
+                        Про школу
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="documents.html">
+                        <span class="mbri-file mbr-iconfont mbr-iconfont-btn"></span>
+                        Документи
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="finance.html">
+                        <span class="mbri-calendar mbr-iconfont mbr-iconfont-btn"></span>
+                        Фінансування
+                    </a>
+                </li>
+            </ul>
+           
+        </div>
+    </nav>
+</section>
+<link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise-gallery/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+   <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smooth-scroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touch-swipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/masonry/masonry.pkgd.min.js"></script>
+  <script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/bootstrap-carousel-swipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/jquery-mb-vimeo_player/jquery.mb.vimeo_player.js"></script>
+  <script src="assets/jarallax/jarallax.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/mobirise-gallery/player.min.js"></script>
+  <script src="assets/mobirise-gallery/script.js"></script>
+  <script src="assets/mobirise-slider-video/script.js"></script>
